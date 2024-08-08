2bf87446ef1dae3743ec18d3bfc0e51ddb9a9f28
Eric Huet <ewhuet@gmail.com>
2024-08-08 21:01:33 +0300

    added the indexRouter and newMessage router to send to the appropriate routes

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const path = require('node:path')
+const { indexRouter } = require( './routes/indexRouter')
+const {newMessage} = require('./routes/newMessageRouter')
 
 const app = express()
 
 app.set('views', path.join(__dirname,'../frontend', 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('index', {message: "sup"})
-})
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter)
+app.use('/new', newMessage)
 
 const PORT = 3000
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
