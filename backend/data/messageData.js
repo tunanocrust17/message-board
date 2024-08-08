0ffc41d6175ef0355139d8eb882f0cfc8634b407
Eric Huet <ewhuet@gmail.com>
2024-08-08 20:59:15 +0300

    created a constant to hold the data array until we can implmenent storage

---
+++ backend/data/messageData.js
@@ -0,0 +1,12 @@
+const messages = [
+    {userName: "bobby",
+        userMessage: "woah this app rocks!",
+        timeStamp: new Date()
+    },
+    {userName: "peppa",
+        userMessage: "everyone loves castles!",
+        timeStamp: new Date()
+    }
+]
+
+module.exports = {messages}
