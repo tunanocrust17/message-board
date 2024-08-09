e741d0972965d1aee17a89ff0fbf5ce3ec227ca8
Eric Huet <ewhuet@gmail.com>
2024-08-09 06:21:19 +0300

    - fixed the way messages are being imported, - added in let to properly define the variables inside the post request

--- backend/routes/newMessageRouter.js
+++ backend/routes/newMessageRouter.js
@@ -1,15 +1,15 @@
 const {Router} = require('express')
 const newMessage = Router()
-const messages = require('../data/messageData')
+const {messages} = require('../data/messageData')
 
 newMessage.get('/', (req, res) => {
     res.render('form', {message: "test"})
 })
 
 newMessage.post('/', (req, res) => {
-    userName = req.body.userName
-    userMessage = req.body.userMessage
-    timeStamp = new Date();
+    let userName = req.body.userName
+    let userMessage = req.body.userMessage
+    let timeStamp = new Date();
     messages.push({userName: userName, userMessage: userMessage, timeStamp: timeStamp})
     res.redirect('/')
 })
