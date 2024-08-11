d42ed33667bf0cc9b0ea2e13e72c62a1b17e53ca
Eric Huet <ewhuet@gmail.com>
2024-08-11 18:00:18 +0300

    added an id field to identify each message

--- backend/data/messageData.js
+++ backend/data/messageData.js
@@ -1,15 +1,18 @@
 const messages = [
     {
+        id: 1, 
         userName: "Rocky",
         userMessage: "Don't lose it, reuse it!",
         timeStamp: new Date()
     },
     {
+        id: 2,
         userName: "peppa",
         userMessage: "everyone loves castles!",
         timeStamp: new Date()
     },
     {
+        id: 3, 
         userName: "Chase",
         userMessage: "Chase is on the case!",
         timeStamp: new Date()
