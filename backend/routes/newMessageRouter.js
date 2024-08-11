6f831bd02c22008b11fe7c62e656e20b2b81e7d3
Eric Huet <ewhuet@gmail.com>
2024-08-11 18:01:34 +0300

    added in the year variable to send to the footer on each page

--- backend/routes/newMessageRouter.js
+++ backend/routes/newMessageRouter.js
@@ -2,15 +2,18 @@ const {Router} = require('express')
 const newMessage = Router()
 const {messages} = require('../data/messageData')
 
+const year = new Date().getFullYear()
+
 newMessage.get('/', (req, res) => {
-    res.render('form', {message: "test"})
+    res.render('form', {message: "test", year: year})
 })
 
 newMessage.post('/', (req, res) => {
+    let newID = messages.length > 0 ? messages[messages.length-1].id + 1 : 1;
     let userName = req.body.userName
     let userMessage = req.body.userMessage
     let timeStamp = new Date();
-    messages.push({userName: userName, userMessage: userMessage, timeStamp: timeStamp})
+    messages.push({id: newID, userName: userName, userMessage: userMessage, timeStamp: timeStamp})
     res.redirect('/')
 })
 
