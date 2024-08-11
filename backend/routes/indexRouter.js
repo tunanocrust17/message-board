2c4a538c5d92cd193e5b32753080257dc2b8f80b
Eric Huet <ewhuet@gmail.com>
2024-08-11 15:25:12 +0300

    added a year variable and passed that into the index page

--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -2,10 +2,10 @@ const {Router} = require('express')
 const indexRouter = Router()
 const {messages} = require('../data/messageData')
 
-
+const year = new Date().getFullYear()
 
 indexRouter.get('/', (req, res) => {
-    res.render('index', {messages: messages})
+    res.render('index', {messages: messages, year: year})
 })
 
 module.exports = {indexRouter}
